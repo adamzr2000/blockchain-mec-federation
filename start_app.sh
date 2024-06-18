b167857cc0eccaeaee24171baa5cb0c4fe5b6ef1
adamzr2000 <adamzr2000@gmail.com>
2024-06-18 14:04:39 +0200

    screena

--- start_app.sh
+++ start_app.sh
@@ -15,5 +15,5 @@ SCREEN_SESSION_NAME="dlt_federation_api"
 # Start a new screen session and run the command
 screen -dmS $SCREEN_SESSION_NAME bash -c "python3 -m uvicorn main:app --reload --host 0.0.0.0 --port 8000 --env-file $FEDERATION_ENV_FILE"
 
-echo "Server started in screen session: $SCREEN_SESSION_NAME"echo "Server started in screen session: $SCREEN_SESSION_NAME"
+echo "Server started in screen session: $SCREEN_SESSION_NAME"
 echo "You can attach to it using: screen -r $SCREEN_SESSION_NAME"
