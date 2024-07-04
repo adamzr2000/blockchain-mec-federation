90fef52c1f99e1d1fc6c6b38117d36bf26380222
adamzr2000 <adamzr2000@gmail.com>
2024-07-04 10:24:41 +0000

    python3.8.10

--- start_app.sh
+++ start_app.sh
@@ -7,13 +7,13 @@ if [ -z "$1" ]; then
 fi
 
 # Load environment variables from the specified file
-export FEDERATION_ENV_FILE=$1
+FEDERATION_ENV_FILE=$1
 
 # Define a screen session name
-SCREEN_SESSION_NAME="dlt_federation_api"
+SCREEN_SESSION_NAME="dlt-federation-api"
 
 # Start a new screen session and run the command
-screen -dmS $SCREEN_SESSION_NAME bash -c "python3 -m uvicorn main:app --reload --host 0.0.0.0 --port 8000 --env-file $FEDERATION_ENV_FILE"
+screen -dmS $SCREEN_SESSION_NAME bash -c "FEDERATION_ENV_FILE=$FEDERATION_ENV_FILE python3 -m uvicorn main:app --reload --host 0.0.0.0 --port 8000 --env-file $FEDERATION_ENV_FILE"
 
 echo "Server started in screen session: $SCREEN_SESSION_NAME"
 echo "You can attach to it using: screen -r $SCREEN_SESSION_NAME"
