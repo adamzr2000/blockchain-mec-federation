9fffee0dcf6dcf92c3a6e05160f6f6efa16954ca
adamzr2000 <adamzr2000@gmail.com>
2024-07-20 17:39:04 +0000

    oo

---
+++ utils/start_dlt_federation_api.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Function to execute a command via SSH and print debug information
+execute_ssh_command() {
+  local node_ip=$1
+  local command=$2
+  echo "Executing on ${node_ip}: ${command}"
+  ssh netcom@${node_ip} "${command}"
+  if [ $? -ne 0 ]; then
+    echo "Error: Command failed on ${node_ip}"
+  else
+    echo "Success: Command executed on ${node_ip}"
+  fi
+}
+
+# Loop to start the app for consumers from node 1 to node 10
+for i in {1..10}; do
+  NODE_IP="10.5.99.${i}"
+  CONFIG_FILE="config/federation/10-offer/consumer${i}.env"
+  execute_ssh_command "${NODE_IP}" "cd /home/netcom/blockchain-mec-federation && ./start_app.sh ${CONFIG_FILE}"
+done
+
+# Loop to start the app for providers from node 11 to node 30
+for i in {11..30}; do
+  NODE_IP="10.5.99.${i}"
+  PROVIDER_INDEX=$((i - 10))
+  CONFIG_FILE="config/federation/10-offer/provider${PROVIDER_INDEX}.env"
+  execute_ssh_command "${NODE_IP}" "cd /home/netcom/blockchain-mec-federation && ./start_app.sh ${CONFIG_FILE}"
+done
