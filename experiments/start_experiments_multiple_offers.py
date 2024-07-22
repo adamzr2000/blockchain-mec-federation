df62791e2b1c5bed38f23adfb8ca48c93dcf5478
adamzr2000 <adamzr2000@gmail.com>
2024-07-22 14:51:33 +0000

    10-offer-cons1

--- experiments/start_experiments_multiple_offers.py
+++ experiments/start_experiments_multiple_offers.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 
 # Constants
-EXPORT_RESULTS = "false"
+EXPORT_RESULTS = "true"
 BASE_URLS = [
     "http://10.5.99.1:8000", "http://10.5.99.2:8000", "http://10.5.99.3:8000", "http://10.5.99.4:8000",
     "http://10.5.99.5:8000", "http://10.5.99.6:8000", "http://10.5.99.7:8000", "http://10.5.99.8:8000",
@@ -16,7 +16,7 @@ BASE_URLS = [
 
 NUM_CONSUMERS = 10
 NUM_PROVIDERS = 20
-NUM_TESTS = 2  # Set the number of tests to run
+NUM_TESTS = 20  # Set the number of tests to run
 
 def generate_prices():
     """ Generate prices for providers as [1, 2, 3, ... 20] """
