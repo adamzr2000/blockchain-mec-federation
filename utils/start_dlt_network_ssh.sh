cb034174b61c19c5f746f2c7ba8abbed319e3b8d
adamzr2000 <adamzr2000@gmail.com>
2024-07-29 17:21:29 +0000

    aaal

--- utils/start_dlt_network_ssh.sh
+++ utils/start_dlt_network_ssh.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Check if a parameter is passed and if it is greater than or equal to 2
+if [ -z "$1" ] || [ "$1" -lt 2 ]; then
+  echo "Error: A mandatory parameter must be provided, and it must be at least 2."
+  exit 1
+fi
+
+# Assign the parameter to a variable
+NODES=$1
+
 # Define the base command and directory
 BASE_COMMAND="cd /home/netcom/blockchain-mec-federation/dlt-network-docker/ &&"
 
@@ -20,11 +29,11 @@ execute_ssh_command() {
 execute_ssh_command "10.5.99.1" "${BASE_COMMAND} ./start_dlt_network.sh"
 
 # Wait for 5 seconds after the first command
-echo "Waiting for 15 seconds..."
-sleep 15
+# echo "Waiting for 5 seconds..."
+# sleep 5
 
-# Loop to join the DLT network from node 2 to node 30
-for i in {2..6}; do
+# Loop to join the DLT network from node 2 to the specified number of nodes
+for ((i=2; i<=NODES; i++)); do
   NODE_IP="10.5.99.${i}"
   NODE_NAME="node${i}"
   execute_ssh_command "${NODE_IP}" "${BASE_COMMAND} ./join_dlt_network.sh ${NODE_NAME} 30"
